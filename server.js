93b90a0f9fa41d9812ae57735adff731ba292e01
Darun <139690402+darunbjork@users.noreply.github.com>
2023-12-14 00:48:07 +0100

    Re-enable token-based authentication

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/users', passport.authenticate('jwt', {session: false}), async (req, re
 
 
 
-app.get('/movies',  async (req, res) => {
+app.get('/movies',  passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
