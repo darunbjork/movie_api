1d949e0447a8491cf7aa876e4075fe21726f26a6
Darun <139690402+darunbjork@users.noreply.github.com>
2023-12-31 11:48:38 +0100

    modifing the Movies endpoint

--- server.js
+++ server.js
@@ -114,6 +114,20 @@ app.get('/users', passport.authenticate('jwt', { session: false }), async (req, 
 
 
 
+// Protected route for fetching movies
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const movies = await Movies.find();
+    res.status(200).json(movies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+/*
+
 app.get('/movies', passport.authenticate('jwt', { session: false }),  async (req, res) => {
   await Movies.find()
     .then((movies) => {
@@ -124,7 +138,7 @@ app.get('/movies', passport.authenticate('jwt', { session: false }),  async (req
       res.status(500).send('Error: ' + error);
     });
 });
-
+*/
 
 
 
